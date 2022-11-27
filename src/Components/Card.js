1d61c190982000bf2897c3d526ecb5b8fa40fcdd
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2022-11-27 10:38:47 +0530

    transition change

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState,  useEffect } from "react";
 import item from '../assets/images/image-product-desktop.jpg';
+import mitem from '../assets/images/image-product-mobile.jpg';
 import cart from '../assets/images/icon-cart.svg';
 import './card.css';
+
 export default function Card(props) {
+    const[mobile, setMobile] = useState(window.innerWidth > 650? false: true);
+    let flag = window.innerWidth > 650? false: true;
+    const handleResize = () => {
+        if(window.innerWidth < 650 && flag===false){
+            flag = true;
+            setMobile(true)
+        } else if (window.innerWidth > 650 && flag===true){
+            flag = false;
+            setMobile(false)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
     return(
         <div className="card">
-            <img className="card-image" src={item} alt="item" />
+            <img className="card-image" src={mobile ? mitem :item} alt="item" />
             <div className="card-content">
                 <div className="category">PERFUME</div>
                 <h1 className="title">Gabrielle Essence Eau De Parfum</h1>
